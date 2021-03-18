c972921daf3cec48952243ff78651b489e540a28
I. Borisoglebskiy <i.borisoglebskiy@outlook.com>
2021-03-18 21:00:42 +0300

    Adding main solution. Adding project for db context.

---
+++ Source/GenSci.FamilyBudget/GenSci.FamilyBudget.DataContext/Models/User.cs
@@ -0,0 +1,33 @@
+﻿namespace GenSci.FamilyBudget.DataContext.Models
+{
+    /// <summary>
+    /// User entity
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// User identifier
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// User first name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// User second name
+        /// </summary>
+        public string SecondName { get; set; }
+
+        /// <summary>
+        /// Password hash
+        /// </summary>
+        public byte[] PassHash { get; set; }
+
+        /// <summary>
+        /// Password salt
+        /// </summary>
+        public byte[] PassSalt { get; set; }
+    }
+}
